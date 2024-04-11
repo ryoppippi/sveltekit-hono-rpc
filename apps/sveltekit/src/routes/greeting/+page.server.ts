5262831c6336138632362ebf9717a9b8521e5034
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-04-11 10:34:18 +0100

    feat(superforms): update from v1 to v2

--- apps/sveltekit/src/routes/greeting/+page.server.ts
+++ apps/sveltekit/src/routes/greeting/+page.server.ts
@@ -1,21 +1,27 @@
 import { getClient } from '$lib/api/client.js';
 import { superValidate, message } from 'sveltekit-superforms/server';
+import { zod } from 'sveltekit-superforms/adapters';
 import { z } from 'zod';
 
 const schema = z.object({
-	name: z.string().default('John Doe'),
-	age: z.number().default(20)
+	name: z.string(),
+	age: z.number()
 });
 
+const defaults = {
+	name: 'John Doe',
+	age: 20
+} as const satisfies z.infer<typeof schema>;
+
 export const load = async () => {
-	const form = await superValidate(schema);
+	const form = await superValidate(zod(schema, { defaults }));
 
 	return { form };
 };
 
 export const actions = {
 	default: async ({ request, fetch }) => {
-		const form = await superValidate(request, schema);
+		const form = await superValidate(request, zod(schema));
 		console.log('POST', form);
 
 		if (!form.valid) {
